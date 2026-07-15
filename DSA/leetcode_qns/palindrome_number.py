df848907c3035d6ab6f4f484c6f4c59efcd32cdb
vishnuvinod001 <kulathoorvishnu@gmail.com>
2026-07-15 22:51:00 +0530

    Implemented the solution to Leetcode:9 - Palindrome Number

---
+++ DSA/leetcode_qns/palindrome_number.py
@@ -0,0 +1,15 @@
+def isPalindrome(x):
+    if x < 0 or (x%10 == 0 and x != 0):
+        return False
+    
+    reversed_half = 0
+    while x > reversed_half:
+        reversed_half = reversed_half * 10 + x % 10
+        x//=10
+    
+    return x == reversed_half or x == reversed_half//10
+
+a = 1212
+print(isPalindrome(a))
+a = 121
+print(isPalindrome(a))
