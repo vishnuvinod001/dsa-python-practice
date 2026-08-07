3ec78e3299846e966aaf9d4ac585a46201482f47
vishnuvinod001 <kulathoorvishnu@gmail.com>
2026-08-07 21:52:12 +0530

    Edited the contents of Leetcode: 1 - Two Sum

--- DSA/Hash_maps/LC1.py
+++ DSA/Hash_maps/LC1.py
@@ -14,19 +14,16 @@ from typing import List
 
 def two_sum(nums: List[int], target: int) -> List[int]:
     seen = {}
-    
     for i, num in enumerate(nums):
         needed = target - num
-        
         if needed in seen:
-            return(seen[needed],i)
-        
+            return(seen[needed], i)
         seen[num] = i
-        
+    
     return []
 
 if __name__ == "__main__":
     nums = [2, 7, 11, 15]
-    target = 9
+    target = 26
     
     print(two_sum(nums, target))
