1af258c1430dcf0ba62ead054a2ee886a991f74a
vishnuvinod001 <kulathoorvishnu@gmail.com>
2026-08-18 21:25:35 +0530

    Implemented the solution to Leetcode: 416 - Partition Equal Subset Sum

---
+++ DSA/DP_Recursion/LC416.py
@@ -0,0 +1,20 @@
+def canPartition(nums):
+    
+    total = sum(nums)
+    
+    if total % 2:
+        return False
+    
+    target = total // 2
+    
+    dp = [False] * (target + 1)
+    
+    dp[0] = True
+    
+    for num in nums:
+        
+        for s in range(target, num - 1, -1):
+            if dp[s - num]:
+                dp[s] = True
+    
+    return dp[target]
