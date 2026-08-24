8337389321027f668859240f83b55c2cc2a3b426
vishnuvinod001 <kulathoorvishnu@gmail.com>
2026-08-24 21:58:35 +0530

    Implemented the solution to Leetcode: 322 - Coin Change

---
+++ DSA/DP_Recursion/LC322.py
@@ -0,0 +1,23 @@
+"""
+Leetcode: 322 - Coin Change
+
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+
+"""
+
+def coinChange(coins, amount):
+    
+    dp = [float('inf')] * (amount + 1)
+    
+    dp[0] = 0
+    
+    for amt in range(1, amount + 1):
+        for coin in coins:
+            if amt - coin >= 0:
+                dp[amt] = min(dp[amt], 1 + dp[amt - coin])
+    
+    return dp[amount] if dp[amount] != float('inf') else -1
