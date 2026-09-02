188b9fea0cd4506549e48ee265cced1df94eab82
vishnuvinod001 <kulathoorvishnu@gmail.com>
2026-09-02 22:17:20 +0530

    Added the solution to Leetcode: 141 - Linked List Cycle

---
+++ DSA/Linked_List/LC141.py
@@ -0,0 +1,17 @@
+def hasCycle(self, head) -> bool:
+
+        if head == None:
+            return False
+
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+        
+        return False
